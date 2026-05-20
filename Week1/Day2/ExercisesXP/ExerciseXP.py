172b355879cee4848895e59f548b1848528207b8
bah862696-coder <bah862696@gmail.com>
2026-05-21 01:22:40 +0200

    Exercise1

---
+++ Week1/Day2/ExercisesXP/ExerciseXP.py
@@ -0,0 +1,24 @@
+# Exercise 1: Converting Lists into Dictionaries
+# Key Python Topics:
+#
+# Creating dictionaries
+# Zip function or dictionary comprehension
+#
+# Instructions
+#
+# You are given two lists. Convert them into a dictionary where the first list contains the keys and the second list contains the corresponding values.
+#
+# Lists:
+#
+# keys = ['Ten', 'Twenty', 'Thirty']
+# values = [10, 20, 30]
+#
+# Expected Output:
+#
+# {'Ten': 10, 'Twenty': 20, 'Thirty': 30}
+
+keys = ['Ten', 'Twenty', 'Thirty']
+values = [10, 20, 30]
+
+result = dict(zip(keys, values))
+print(result)
