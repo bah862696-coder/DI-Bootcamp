13676ab193d2e00e39fee43843da7a952c2d0b89
bah862696-coder <bah862696@gmail.com>
2026-05-23 02:47:43 +0200

    Fix daily challenge

--- Week1/Day4/ExercisesXP/ExerciseXP.py
+++ Week1/Day4/ExercisesXP/ExerciseXP.py
@@ -78,3 +78,54 @@ dog3 = Dog("Buddy", 2, 15)
 print(dog1.bark())
 print(dog2.run_speed())
 print(dog1.fight(dog2))
+#
+#
+# Exercise 3 : Pet Dogs
+import random
+class Dog:
+    # Constructor
+    def __init__(self, name, age, weight):
+        self.name = name
+        self.age = age
+        self.weight = weight
+    # Bark method
+    def bark(self):
+        return f"{self.name} is barking"
+
+# Child class : PetDog
+class PetDog(Dog):
+    # Constructor
+    def __init__(self, name, age, weight):
+        # Inherit attributes from Dog
+        super().__init__(name, age, weight)
+        self.trained = False
+    # Train method
+    def train(self):
+        print(self.bark())
+        self.trained = True
+    # Play method
+    def play(self, *args):
+        dog_names = [self.name]
+        for dog in args:
+            dog_names.append(dog.name)
+        print(f"{', '.join(dog_names)} all play together")
+    # Random trick method
+    def do_a_trick(self):
+        if self.trained:
+            tricks = [
+                "does a barrel roll",
+                "stands on his back legs",
+                "shakes your hand",
+                "plays dead"
+            ]
+            print(f"{self.name} {random.choice(tricks)}"
+# Step 3 : Test PetDog methods
+dog1 = PetDog("Fido", 2, 10)
+dog2 = PetDog("Buddy", 3, 15)
+dog3 = PetDog("Max", 4, 20)
+# Train dog
+dog1.train()
+# Dogs play together
+dog1.play(dog2, dog3)
+# Dog does a trick
+dog1.do_a_trick()
