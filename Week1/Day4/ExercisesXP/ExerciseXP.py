077bf059956943a59bcd9e7b8204198b073b9606
bah862696-coder <bah862696@gmail.com>
2026-05-23 02:35:46 +0200

    Fix daily challenge

--- Week1/Day4/ExercisesXP/ExerciseXP.py
+++ Week1/Day4/ExercisesXP/ExerciseXP.py
@@ -45,4 +45,36 @@ sara_pets = Pets(all_cats)
 sara_pets.walk()
 #
 #
-#
+# Exercise 2 : Dogs
+# Step 1 : Create the Dog class
+class Dog:
+    # Constructor
+    def __init__(self, name, age, weight):
+        self.name = name
+        self.age = age
+        self.weight = weight
+    # Bark method
+    def bark(self):
+        return f"{self.name} is barking"
+    # Run speed method
+    def run_speed(self):
+        return (self.weight / self.age) * 10
+    # Fight method
+    def fight(self, other_dog):
+        my_power = self.run_speed() * self.weight
+        other_power = other_dog.run_speed() * other_dog.weight
+        if my_power > other_power:
+            return f"{self.name} wins the fight against {other_dog.name}"
+        elif other_power > my_power:
+            return f"{other_dog.name} wins the fight against {self.name}"
+        else:
+            return "It's a tie!"
+
+# Step 2 : Create dog instances
+dog1 = Dog("Rocky", 4, 20)
+dog2 = Dog("Max", 5, 25)
+dog3 = Dog("Buddy", 2, 15)
+# Step 3 : Test methods
+print(dog1.bark())
+print(dog2.run_speed())
+print(dog1.fight(dog2))
