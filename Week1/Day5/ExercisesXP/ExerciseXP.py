37ff770ec57c99812189d04e3d5687140c31e782
bah862696-coder <bah862696@gmail.com>
2026-05-24 12:28:56 +0200

    Fix daily challenge

--- Week1/Day5/ExercisesXP/ExerciseXP.py
+++ Week1/Day5/ExercisesXP/ExerciseXP.py
@@ -40,3 +40,40 @@ class Game:
         else:
             print(" Match nul !")
         return result
+        #
+        #
+        #from game import Game
+def get_user_menu_choice():
+    print("\n=== MENU ===")
+    print("(g) Jouer")
+    print("(s) Voir les scores")
+    print("(q) Quitter")
+    choice = input("Votre choix : ").lower()
+    while choice not in ["g", "s", "q"]:
+        print("Choix invalide.")
+        choice = input("Votre choix : ").lower()
+    return choice
+def print_results(results):
+    print("\n===== SCORES =====")
+    print(f"Victoires : {results['win']}")
+    print(f"Défaites : {results['loss']}")
+    print(f"Matchs nuls : {results['draw']}")
+    print("\nMerci d'avoir joué ")
+def main():
+    results = {
+        "win": 0,
+        "loss": 0,
+        "draw": 0
+    }
+    while True:
+        user_choice = get_user_menu_choice()
+        if user_choice == "g":
+            game = Game()
+            result = game.play()
+            results[result] += 1
+        elif user_choice == "s":
+            print_results(results)
+        elif user_choice == "q":
+            print_results(results)
+            break
+main()
